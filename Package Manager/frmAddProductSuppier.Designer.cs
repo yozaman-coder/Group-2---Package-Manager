2f655c2cba9563308e0ccc30262164afcaf4e549
yozaman-coder <jamesstraka5@gmail.com>
2022-01-18 14:08:33 -0700

    Add project files.

---
+++ Package Manager/frmAddProductSuppier.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Package_Manager
+{
+    partial class frmAddProductSuppier
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboProducts = new System.Windows.Forms.ComboBox();
+            this.lblProducts = new System.Windows.Forms.Label();
+            this.lblSupplier = new System.Windows.Forms.Label();
+            this.cboSuppliers = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboProducts
+            // 
+            this.cboProducts.FormattingEnabled = true;
+            this.cboProducts.Location = new System.Drawing.Point(19, 36);
+            this.cboProducts.Name = "cboProducts";
+            this.cboProducts.Size = new System.Drawing.Size(121, 23);
+            this.cboProducts.TabIndex = 0;
+            // 
+            // lblProducts
+            // 
+            this.lblProducts.AutoSize = true;
+            this.lblProducts.Location = new System.Drawing.Point(19, 18);
+            this.lblProducts.Name = "lblProducts";
+            this.lblProducts.Size = new System.Drawing.Size(54, 15);
+            this.lblProducts.TabIndex = 1;
+            this.lblProducts.Text = "Products";
+            // 
+            // lblSupplier
+            // 
+            this.lblSupplier.AutoSize = true;
+            this.lblSupplier.Location = new System.Drawing.Point(156, 18);
+            this.lblSupplier.Name = "lblSupplier";
+            this.lblSupplier.Size = new System.Drawing.Size(50, 15);
+            this.lblSupplier.TabIndex = 2;
+            this.lblSupplier.Text = "Supplier";
+            // 
+            // cboSuppliers
+            // 
+            this.cboSuppliers.Enabled = false;
+            this.cboSuppliers.FormattingEnabled = true;
+            this.cboSuppliers.Location = new System.Drawing.Point(156, 36);
+            this.cboSuppliers.Name = "cboSuppliers";
+            this.cboSuppliers.Size = new System.Drawing.Size(121, 23);
+            this.cboSuppliers.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add new product";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(156, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add new supplier";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(85, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 41);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Add to selected package";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // frmAddProductSuppier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 178);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cboSuppliers);
+            this.Controls.Add(this.lblSupplier);
+            this.Controls.Add(this.lblProducts);
+            this.Controls.Add(this.cboProducts);
+            this.Name = "frmAddProductSuppier";
+            this.Text = "Add Product";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboProducts;
+        private System.Windows.Forms.Label lblProducts;
+        private System.Windows.Forms.Label lblSupplier;
+        private System.Windows.Forms.ComboBox cboSuppliers;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
