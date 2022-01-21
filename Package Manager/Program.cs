6936b5ea52ab55f292d836ac1cd221ef64a402b8
yozaman-coder <jamesstraka5@gmail.com>
2022-01-21 11:10:27 -0700

    Created server connection

--- Package Manager/Program.cs
+++ Package Manager/Program.cs
@@ -17,7 +17,7 @@ namespace Package_Manager
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmAddProductSuppier());
+            Application.Run(new frmAddPackage());
         }
     }
 }
